52248095c1ac9a9395896762cc4c78a74adc2050
Andy Fleener <andrew.fleener@sportngin.com>
2013-12-22 12:21:06 -0600

    Fix metadata versions

--- metadata.rb
+++ metadata.rb
@@ -6,8 +6,8 @@ description      'Installs/Configures sportngin-mongodb'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-recipe "spotngin-mongodb"
-recipe "sportngin-mongodb::mongodb_repo"
+recipe "spotngin-mongodb", "Installs and configures the most recent verison of mongodb"
+recipe "sportngin-mongodb::mongodb_repo", "Enables the mongodb package reopo"
 
 depends "yum"
-depends "mongo", ">= 0.14"
+depends "mongodb", ">= 0.13"
