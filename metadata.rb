975896838345c6c6ba3c6dac0590198c3164ef6d
Andy Fleener <andrew.fleener@sportngin.com>
2013-12-22 11:56:32 -0600

    Update metadata.rb for the recipe list

--- metadata.rb
+++ metadata.rb
@@ -1,4 +1,4 @@
-name             'sportngin-mongodb-cookbook'
+name             'sportngin-mongodb'
 maintainer       'Sport Ngin, Inc.'
 maintainer_email 'infrastructure@sportngin.com'
 license          'All rights reserved'
@@ -6,4 +6,8 @@ description      'Installs/Configures sportngin-mongodb-cookbook'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
+recipe "spotngin-mongodb"
+recipe "sportngin-mongodb::mongodb_repo"
+
+depends "yum"
 depends "mongo", ">= 0.14"
